1dac21fb7d25b0265c36426c056a6c8b666997f4
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-07-18 20:03:01 +0300

    Created ToDoItem class

---
+++ SampleWebApp/Models/ToDoItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SampleWebApp.Models
+{
+	public class ToDoItem
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+		
+		private int _priority;
+		
+		public int Priority
+		{
+			get { return _priority; }
+			set
+			{
+				if (value > 0 && value < 6)
+					_priority = value;
+				else
+					_priority = 3;
+			}
+		}
+
+		public ToDoItem(int id, string name, string description, int priority)
+		{
+			Id = id;
+			Name = name;
+			Description = description;
+			Priority = priority;
+		}
+	}
+}
