3379a2e89967dc6c7cd3e7ca69668979fbebc81d
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-08-02 16:37:47 +0300

    Created Tag class

---
+++ SampleWebApp/Models/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleWebApp.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public Tag()
+        {
+
+        }
+    }
+}
