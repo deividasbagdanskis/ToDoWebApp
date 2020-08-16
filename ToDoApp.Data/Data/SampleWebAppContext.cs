569a7ee1da7ec7d436a1742f4e54880cf77b573e
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-08-16 21:26:38 +0300

    Moved Dao models, context, migrations to ToDoApp.Data

---
+++ ToDoApp.Data/Data/SampleWebAppContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data.Models;
+
+namespace ToDoApp.Data.Data
+{
+    public class SampleWebAppContext : DbContext
+    {
+        public SampleWebAppContext (DbContextOptions<SampleWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItemTagDao>().HasKey(tt => new { tt.ToDoItemId, tt.TagId });
+
+            modelBuilder.Entity<ToDoItemTagDao>()
+                .HasOne<ToDoItemDao>(tt => tt.ToDoItem)
+                .WithMany(t => t.ToDoItemTags)
+                .HasForeignKey(tt => tt.ToDoItemId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ToDoItemTagDao>()
+                .HasOne<TagDao>(tt => tt.Tag)
+                .WithMany(t => t.ToDoItemTags)
+                .HasForeignKey(tt => tt.TagId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        public DbSet<CategoryDao> Category { get; set; }
+
+        public DbSet<ToDoItemDao> ToDoItem { get; set; }
+
+        public DbSet<ToDoItemTagDao> ToDoItemTag { get; set; }
+
+        public DbSet<TagDao> Tag { get; set; }
+    }
+}
---
+++ ToDoApp.Data/Models/CategoryDao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using ToDoApp.Commons.Interfaces;
+
+namespace ToDoApp.Data.Models
+{
+    [Table("Category")]
+    public class CategoryDao : IHasId
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<ToDoItemDao> ToDoItems { get; set; }
+
+        public CategoryDao()
+        {
+
+        }
+
+        public CategoryDao(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
