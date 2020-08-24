727ae90b96a1b467b2ff8fa59e19502cc672dc23
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-08-24 21:39:48 +0300

    Created Client controller and db context

---
+++ ToDoApp/ToDoApp.Projects.Api/Data/ToDoAppProjectsApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Projects.Api.Models;
+
+namespace ToDoApp.Projects.Api.Data
+{
+    public class ToDoAppProjectsApiContext : DbContext
+    {
+        public ToDoAppProjectsApiContext (DbContextOptions<ToDoAppProjectsApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+    }
+}
--- ToDoApp/ToDoApp.Projects.Api/Startup.cs
+++ ToDoApp/ToDoApp.Projects.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Projects.Api.Data;
 
 namespace ToDoApp.Projects.Api
 {
@@ -26,6 +28,9 @@ namespace ToDoApp.Projects.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ToDoAppProjectsApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ToDoAppProjectsApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
