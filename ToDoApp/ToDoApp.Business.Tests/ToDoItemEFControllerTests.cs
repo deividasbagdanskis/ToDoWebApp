c3e93419ab342ad52bc66ce5c882c2f779359664
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-09-06 19:24:29 +0300

    Created ToDoApp.Business.Tests project

---
+++ ToDoApp/ToDoApp.Business.Tests/ToDoItemEFControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ToDoApp.Business.Tests
+{
+    public class ToDoItemEFControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
