c686186162b4ac612f747ef869f9d15f809d7330
Deividas Bagdanskis <51085446+deividasbagdanskis@users.noreply.github.com>
2020-09-19 14:15:04 +0300

    Created UI tests for Category views

---
+++ ToDoApp/ToDoApp.Web.Tests/PageObjects/CategoryPages/CreateCategoryPage.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+
+namespace ToDoApp.Web.Tests.PageObjects.CategoryPages
+{
+    class CreateCategoryPage
+    {
+        private readonly IWebDriver webDriver;
+
+        private readonly string createCategoryPageUrl = "https://localhost:44392/CategoriesEF/Create";
+        private readonly By categotyNameInput = By.Id("Name");
+        private readonly By createCategotyButton = By.XPath("/html/body/div/main/div[1]/div/form/div[2]/input");
+        private readonly By categoryNameError = By.XPath("/html/body/div/main/div[1]/div/form/div[1]");
+
+        public CreateCategoryPage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public void CreateCategory(string name)
+        {
+            webDriver.Navigate().GoToUrl(createCategoryPageUrl);
+
+            webDriver.FindElement(categotyNameInput).SendKeys(name);
+            webDriver.FindElement(createCategotyButton).Click();
+        }
+
+        public string GetCreatedCategoryName(string name)
+        {
+            By createdCategoryRecord = By.XPath($"//td[normalize-space() = '{name}']");
+
+            string createdCategoryName = "";
+
+            try
+            {
+                IWebElement element = webDriver.FindElement(createdCategoryRecord);
+                
+                createdCategoryName = element.Text;
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+
+            return createdCategoryName;
+        }
+
+        public string GetCategoryNameErrorMessage()
+        {
+            string errorMessage = "";
+
+            try
+            {
+                errorMessage = webDriver.FindElement(categoryNameError).Text;
+            }
+            catch (NoSuchElementException)
+            {
+
+            }
+
+            return errorMessage;
+        }
+    }
+}
